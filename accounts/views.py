ed4e61bf4dfb315b3759c98a8fa9df3dfa347390
HYE <jelly12paw@gmail.com>
2022-11-17 17:47:20 +0900

    create signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+# from .models import User 
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'accounts/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:   
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
