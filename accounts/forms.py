ea2f874271583bbd688a81f54b7951edc4c04d1b
HYE <jelly12paw@gmail.com>
2022-11-19 18:58:19 +0900

    design signup.html & edit signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 # from .models import User
 from django.contrib.auth import get_user_model
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
 
+    username = forms.CharField(
+    label ='아이디',
+    widget=forms.TextInput(attrs={
+        'placeholder' : '아이디'
+    }))
+
+    TITLE_CHOICES = [      
+        ('남성', '남성'),
+        ('여성', '여성'),
+        ]
+
+    gender = forms.ChoiceField(
+        label='성별',
+        choices=TITLE_CHOICES
+    )
+
     class Meta:
         model = get_user_model()
-        fields = ('username', 'email', 'gender')
+        fields = ('username', 'gender')
 
 class CustomUserChangeForm(UserChangeForm):
 
